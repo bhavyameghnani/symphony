f7e3ab7bc7bfcc6c9083ab28dec17804543ee1fe
bhavyameghnani <meghnani.bhavya@gmail.com>
2022-04-10 08:19:19 +0530

    restructuring of project folders

--- Symphony-Native/src/screens/Scorecards.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import styled, { css } from "styled-components";
-
-function Scorecards(props) {
-  return <></>;
-}
-
-export default Scorecards;
--- Symphony-Native/src/screens/Untitled.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import styled, { css } from "styled-components";
-
-function Untitled(props) {
-  return <></>;
-}
-
-export default Untitled;
